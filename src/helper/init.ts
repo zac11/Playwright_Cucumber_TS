21af9b8d230c880654bab657a2ee196ee90d3a66
Rahul राहुल <rahul_yadav@external.mckinsey.com>
2023-04-05 16:03:13 +0530

    Added for multiple envs

--- src/helper/init.ts
+++ src/helper/init.ts
@@ -1,8 +1,8 @@
 const fs = require("fs-extra");
 
 try {
-    fs.ensureDir('test-results');
-    fs.emptyDir("test-results");
+  fs.ensureDir("test-results");
+  fs.emptyDir("test-results");
 } catch (error) {
-    console.log(`Unable to create folder with error--> ${error}`);
+  console.log(`Unable to create folder with error--> ${error}`);
 }
