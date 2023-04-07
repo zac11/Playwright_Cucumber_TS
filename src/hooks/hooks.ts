3708d4711f7eb423751d78996202e119d75715c8
Rahul राहुल <rahul_yadav@external.mckinsey.com>
2023-04-07 16:55:56 +0530

    Added to host gh pages for reports

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -3,18 +3,23 @@ import { chromium, Browser, Page, BrowserContext } from "@playwright/test";
 import { invokeBrowser } from "../helper/browsers/browserManager";
 import { pageFixture } from "./pageFixture";
 import { getEnv } from "../helper/env/env";
+import * as fs from 'fs'
 
 let page: Page;
 let browser: Browser;
 let context: BrowserContext;
 
 BeforeAll(async function () {
-    getEnv();
+  getEnv();
   browser = await invokeBrowser();
 });
 
 Before(async () => {
-  context = await browser.newContext();
+  context = await browser.newContext({
+    recordVideo: {
+      dir: "./test-results/videos"
+    }
+  });
   page = await context.newPage();
   pageFixture.page = page;
 });
@@ -22,16 +27,25 @@ Before(async () => {
 After(async function ({ pickle, result }) {
   // take screenshot on failure of a step
   await console.log(result?.status);
+  let videoPath : string;
+  let img : Buffer
   if (result?.status == Status.FAILED) {
-    const img = await pageFixture.page.screenshot({
+    img = await pageFixture.page.screenshot({
       path: `./test-results/screenshots/${pickle.name}.png`,
       type: "png",
     });
-    await this.attach(img, "image/png");
+
+    videoPath = await pageFixture.page.video().path();
+    
   }
 
   await pageFixture.page.close();
   await context.close();
+
+  if(result?.status == Status.FAILED){
+    await this.attach(img, "image/png");
+    await this.attach(fs.readFileSync(videoPath), 'video/webm');
+  }
 });
 
 AfterAll(async function () {
